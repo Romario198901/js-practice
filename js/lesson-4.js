ad50dbb634257922f39bd3b9e922e859d53238d4
Roman Hrydin <romario198901@gmail.com>
2025-11-03 12:52:25 +0200

    complete task3

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -94,3 +94,87 @@ for (let i = 0; i < 100; i++) {
   markup.push(divEl);
 }
 container.append(...markup);
+
+// Form Events, Input, Focus, Blur and Submit.
+
+// Використовуй шаблон форми з файлу html.
+
+// 1 - При події `input`, якщо користувач ввів в поле більше 
+// 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+// то клас `error`
+
+// 2 - При події `focus` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
+
+// 3 - При події `blur` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`, 
+// якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+// 4 - При події `submit`. Відміни поведінку браузера по змовчуванню.
+// Дістань данні з інпуту і чек боксу, зроби перевірку, 
+// що інпут не порожній, також, що нажатий чек бокс у положення true,
+// якщо користувач все виконав вірно, збери данні (userName)
+// у обьект і виведи у консоль. У разі, якщо користувач не виконав
+// одну із умов, виведи повідомлення. Також при події інпут реалізуй додавання 
+// ім`я користувача у span, замість слова "Anonymous".
+// Якщо користувач ввів ім`я, а потім видалив, зроби так,
+// щоб на місце повернулось дефолтне знаяення "Anonymous".
+// При відправці форми, очисти інпут, верни чек бокс у положення 
+// false, верни дефолтне значення "Anonymous" у span.
+
+const form = document.querySelector('.js-contact-form');
+const input = document.querySelector('.js-username-input');
+const checkbox = document.querySelector('.js-policy-checkbox');
+const output = document.querySelector('.js-username-output');
+input.addEventListener('input', () => {
+  if(input.value.length < 6) {
+    input.classList.add('error');
+    output.textContent = 'Anonymous';
+  }
+  else {
+    input.classList.remove('error');
+    input.classList.add('success');
+output.textContent = input.value;
+
+  }
+});
+input.addEventListener('focus', () => {
+  if(input.value.length === 0) {
+    input.style.outline = '3px solid red';
+  }
+  else {
+   input.style.outline = '3px solid green';
+  }
+});
+input.addEventListener('blur', () => {
+  if(input.value.length === 0) {
+    input.style.outline = '3px solid red';
+  }
+  else {
+   input.style.outline = '3px solid lime';
+  }
+});
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const userName = form.elements.userName.value.trim();
+  const isAccepted = form.elements.accept.checked;
+
+  if (userName === '' || !isAccepted) {
+    console.log('Fill the fields');
+    output.textContent = 'Anonymous';
+    return;
+  }
+
+  const userData = {
+    name: userName,
+    accept: isAccepted
+  };
+
+  console.log(userData);
+  output.textContent = userName;
+
+  form.reset(); 
+  output.textContent = 'Anonymous';
+});
